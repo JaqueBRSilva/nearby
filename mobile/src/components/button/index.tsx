c277d9d71bfba7c62bd6f0409383488cfd073b99
Jaqueline Broccolo <j.silvabroccolo@gmail.com>
2024-12-15 21:03:00 -0300

    feat: criado componente 'Button' com `Design Pattern`

---
+++ mobile/src/components/button/index.tsx
@@ -0,0 +1,46 @@
+import { colors } from "@/styles/colors";
+import { IconProps as TablerIconProps } from '@tabler/icons-react-native';
+import { ActivityIndicator, Text, TextProps, TouchableOpacity, TouchableOpacityProps } from "react-native";
+import { buttonStyle } from "./styles";
+
+type ButtonProps = TouchableOpacityProps & {
+    isLoading?: boolean
+}
+
+type IconProps = {
+    icon: React.ComponentType<TablerIconProps>
+}
+
+function Button({ children, style, isLoading = false, ...rest }: ButtonProps) {
+    return (
+        <TouchableOpacity
+            style={[buttonStyle.container, style]}
+            activeOpacity={0.8}
+            disabled={isLoading}
+            {...rest}
+        >
+            {isLoading ? (
+                <ActivityIndicator size={"small"} color={colors.gray[100]} />
+            ) : (
+                children
+            )}
+        </TouchableOpacity>
+    )
+}
+
+function Title({ children }: TextProps) {
+    return (
+        <Text style={buttonStyle.title}>
+            {children}
+        </Text>
+    )
+}
+
+function Icon({ icon: Icon }: IconProps) {
+    return <Icon size={24} color={colors.gray[100]} />
+}
+
+Button.Title = Title
+Button.Icon = Icon
+
+export { Button }
