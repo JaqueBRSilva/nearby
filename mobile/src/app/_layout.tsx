edeb149aed7e01b5acd61f5301c0724a724f2436
Jaqueline Broccolo <j.silvabroccolo@gmail.com>
2024-12-17 22:34:32 -0300

    feat: add `GestureHandlerRootView` ao redor da `Stack`

--- mobile/src/app/_layout.tsx
+++ mobile/src/app/_layout.tsx
@@ -1,12 +1,13 @@
-import { colors } from '@/styles/theme';
-import { Stack } from "expo-router";
 import Loading from "@/components/loading";
+import { colors } from '@/styles/theme';
 import {
     Rubik_500Medium,
     Rubik_600SemiBold,
     Rubik_700Bold,
     useFonts
 } from "@expo-google-fonts/rubik";
+import { Stack } from "expo-router";
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 export default function Layout() {
     const [fontsLoaded] = useFonts({
@@ -20,11 +21,13 @@ export default function Layout() {
     }
 
     return (
-        <Stack
-            screenOptions={{
-                headerShown: false,
-                contentStyle: { backgroundColor: colors.gray[100] },
-            }}
-        />
+        <GestureHandlerRootView style={{ flex: 1 }}>
+            <Stack
+                screenOptions={{
+                    headerShown: false,
+                    contentStyle: { backgroundColor: colors.gray[100] },
+                }}
+            />
+        </GestureHandlerRootView>
     )
 }
